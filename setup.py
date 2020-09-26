0f5518a0eacf3d4958ea9882165fa7943053db15
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-09-25 21:41:56 -0500

    docs: Add init.py to set as modules

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ project_urls = {
 
 setuptools.setup(
     name='ISO8583-Dlib',
-    version='0.1.1',
+    version='0.1.2',
     author='eocode',
     author_email='hola@eliasojedamedina.com',
     license='MIT license',
